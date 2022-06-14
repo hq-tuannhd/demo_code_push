a1be257b188f2b076ea6f854444ae67d5b344493
hq-tuannhd <106048426+hq-tuannhd@users.noreply.github.com>
2022-06-14 11:35:27 +0700

    Add files via upload
    update source flutter

---
+++ android/app/src/main/java/vn/com/hqsoft/demo_code_push/MainActivity.java
@@ -0,0 +1,6 @@
+package vn.com.hqsoft.demo_code_push;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/tuannhd/Desktop/WorkProjects/Flutter/flutter 2.2.3"
+export "FLUTTER_APPLICATION_PATH=/Users/tuannhd/Desktop/WorkProjects/Flutter/WorkProject/demo_code_push"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
